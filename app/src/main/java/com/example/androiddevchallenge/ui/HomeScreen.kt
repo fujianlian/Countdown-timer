92068ba8dc1d6eb2c4b4725cfc095354b04dd872
lianbq <lianbinqiang@wxblockchain.com>
2021-03-04 16:03:16 +0800

    Solve lint error

--- app/src/main/java/com/example/androiddevchallenge/ui/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/HomeScreen.kt
@@ -15,7 +15,13 @@
  */
 package com.example.androiddevchallenge.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
